5bb252fd20d288ecf345ab82b07c9a56c651952d
Funnyyjustin <justin.timmer@live.nl>
2021-10-27 14:48:52 +0200

    Camera (sub)class aangemaakt

--- Engine/Camera.cs
+++ Engine/Camera.cs
@@ -1,7 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
-    class Camera
+namespace Engine
+{
+    class Camera : ExtendedGame
     {
+        public Point WindowBox
+        {
+            get
+            {
+                Point windowBox = windowSize;
+                return windowBox;
+            }
+        }
     }
+}
